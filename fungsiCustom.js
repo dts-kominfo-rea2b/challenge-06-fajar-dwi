ed7e6387a53f431033aa9d047b96f673bf7661ef
Fajar <fajardwi@its.ac.id>
2022-06-12 19:36:36 +0700

    Assign Challenge

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,45 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaFile1 = (file1) => {
+
+};
+
+const bacaData = (fnCallback) => {
+  let err = false;
+  let data = [];
+  
+  fs.readFile(file1, {encoding : 'utf8'}, (error, datafile) => {
+    if(error){
+      err = error;
+    }
+
+    let ArrayOfObject = JSON.parse(datafile);
+    data.push(ArrayOfObject.message.split(" ")[1])
+  });
+
+  fs.readFile(file2, {encoding : 'utf8'}, (error, datafile) => {
+    if(error){
+      err = error;
+    }
+
+    let ArrayOfObject = JSON.parse(datafile);
+    data.push(ArrayOfObject[0].message.split(" ")[1])
+
+  });
+
+  fs.readFile(file3, {encoding : 'utf8'}, (error, datafile) => {
+    if(err){
+      err = error;
+      fnCallback(err, data);
+    }
+
+    let ArrayOfObject = JSON.parse(datafile);
+    data.push(ArrayOfObject[0].data.message.split(" ")[1])
+    fnCallback(err, data);
+  });
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
